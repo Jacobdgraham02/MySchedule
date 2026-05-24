187754ffd54263e800359a646059bc705bb0c64f
Jacob Graham <jacobdgraham02@gmail.com>
2026-05-24 01:02:16 -0400

    Completed local Room database entity class with documentation

--- app/src/main/java/com/jacobdgraham/myschedule/data/local/ShiftEntity.kt
+++ app/src/main/java/com/jacobdgraham/myschedule/data/local/ShiftEntity.kt
@@ -3,6 +3,17 @@ package com.jacobdgraham.myschedule.data.local
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Local Room entity representing a single shift
+ *
+ * Each [ShiftEntity] is stored in the 'shifts' table
+ *
+ * @property date the unique date for each shift that is worked. I only work a maximum of one time per day, so there will only be 1 shift value for each date
+ *
+ * @property shiftCode the code representing the type of shift worked on this date. This date will be formatted in 24-hour format, where the first two digits
+ * represent the starting shift hour, and the last two digits represent the length of the shift. For example, 2010 represents a shift that starts at 8:00 p.m.
+ * and ends at 6:00 a.m.
+ */
 @Entity(tableName="shifts")
 data class ShiftEntity(
     @PrimaryKey val date: String,
