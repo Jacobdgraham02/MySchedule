6f60551dce16d76e0a3c2c786adffb28e0cbbc7e
Jacob Graham <jacobdgraham02@gmail.com>
2026-05-27 10:26:37 -0400

    Created file which defines local Room database

--- app/src/main/java/com/jacobdgraham/myschedule/data/local/AppDatabase.kt
+++ app/src/main/java/com/jacobdgraham/myschedule/data/local/AppDatabase.kt
@@ -1,4 +1,17 @@
 package com.jacobdgraham.myschedule.data.local
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+/**
+ * Declaration for local Room database which defines which entities exist in the database and which DAOs are available
+ */
+@Database(
+    entities =[ShiftEntity::class],
+    version =1,
+    exportSchema = false
+)
+
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun shiftDao(): ShiftDao
 }
