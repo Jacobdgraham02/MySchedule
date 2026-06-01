d733ca6c86f3993e24fd6a4f02976f519d6eeae4
Jacob Graham <jacobdgraham02@gmail.com>
2026-06-01 19:02:19 -0400

    Created domain level classes

--- app/src/main/java/com/jacobdgraham/myschedule/domain/model/MonthSchedule.kt
+++ app/src/main/java/com/jacobdgraham/myschedule/domain/model/MonthSchedule.kt
@@ -1,4 +1,16 @@
 package com.jacobdgraham.myschedule.domain.model
 
-class MonthSchedule {
-}
+/**
+ * Represents all shift information for a single month
+ *
+ * A [MonthSchedule] groups together the shift days that belong to the same year and month
+ *
+ * @property year the calendar year, such as 2026
+ * @property month the calendar month, such as 05. This ranges from 1 to 12
+ * @property days the list of days in this month and their shifts
+ */
+data class MonthSchedule (
+    val year: Int,
+    val month: Int,
+    val days: List<ShiftDay>
+)
--- app/src/main/java/com/jacobdgraham/myschedule/domain/model/ShiftDay.kt
+++ app/src/main/java/com/jacobdgraham/myschedule/domain/model/ShiftDay.kt
@@ -1,4 +1,14 @@
 package com.jacobdgraham.myschedule.domain.model
 
-class ShiftDay {
-}
+/**
+ * Represents a given day
+ *
+ * [ShiftDay] is a domain model class used by this application's business and UI layers and separate from the local Room database or any Room entities
+ *
+ * @property date the date of this shift day, formatted as yyyy-mm-dd. For example, 2026-05-20
+ * @property shiftCode the shift code for this day, such as 2110, or null. Null will be used for when there is no shift on that date
+ */
+data class ShiftDay (
+    val date: String,
+    val shiftCode: String?
+)
